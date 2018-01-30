3935e02039c56589c350c1d1e57e3e16c0d9391d
EgorSys <31861436+EgorSys@users.noreply.github.com>
2018-01-30 22:45:45 +0300

    Update Handler.py

--- Handler.py
+++ Handler.py
@@ -27,19 +27,22 @@ schedule = {
 
 
 def handle_message(message, nickname="user"):
-    '''handles message: 
-    @message - text of recieved message
-    @nickname - nickname of sender
-    @returns - text of response
-    '''
-    '''message = updates["response"][len(updates["response"])-1]["text"]
-    nickname = updates["response"][len(updates["response"])-1]["nickname"]
-    chatid = updates["response"][len(updates["response"])-1]["chat_id"]
-    answer = requests.get("https://api.telegram.org/bot<485196249:AAGOPYcyVHFAefxo2p8JJeKQEQ_Y2C2RSSo>/sendMessage?chat_id=chatid&text=message")
-    return answer'''
     if message[:5] == "Что в":
         day = message.split()[2][:len(message.split()[2])-1]
         return schedule[day]["lessons"]
+    if message[:20] == "Когда заканчивается ":
+        if messgage[22] == " ":
+            endtime = lesson_time[int(messgage[21])][1]
+        else:
+            endtime = lesson_time[int(message[20:22])][1]
+        return 'в '+str(endtime)
+    if message[:16] == "Сколько уроков в":
+        day = message.split()[3]
+        number = len(schedule[day][lessons])
+        return number
+    if message[:6] == "Когда " and int(message[6]) < 10:
+        starttime = lesson_time[int(message.split()[1]])][0]
+        return 'в '+str(starttime)
 
 if __name__ == "__main__":
     # dirty python magic, will talk about on the next lesson
@@ -50,4 +53,7 @@ if __name__ == "__main__":
     while True:
         msg = input("Your message: ")
         ans = handle_message(msg, nick)
-        print(*ans, sep = ', ')
+        if msg[:5] == "Что в":
+            print(*ans, sep = ', ')
+        else:
+            print(ans)
